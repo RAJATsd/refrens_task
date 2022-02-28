b58d129d722eac493ecfa187c9b84b7c63747f98
RAJATsd <rajatsharmaatri@gmail.com>
2022-02-28 21:07:45 +0530

    made some changes for deployment

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const dummyData = require("./dummydata");
 const API_PORT = process.env.PORT || "3001";
 
-app.use("/suggestions/:searchText", (req, res) => {
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("/suggestions/:searchText", (req, res) => {
   try {
     const { searchText } = req.params;
     if (!searchText || searchText.trim() === "") {
@@ -32,7 +35,7 @@ app.use("/suggestions/:searchText", (req, res) => {
   }
 });
 
-app.use("/user/:userId", (req, res) => {
+app.get("/user/:userId", (req, res) => {
   try {
     const { userId } = req.params;
     if (!userId || userId === "") {
@@ -43,17 +46,17 @@ app.use("/user/:userId", (req, res) => {
       (singleItem) => singleItem.id === userId
     );
 
-    return res
-      .status(200)
-      .json({
-        message: "RESULT FOUND",
-        data: searchedUser
-      });
+    return res.status(200).json({
+      message: "RESULT FOUND",
+      data: searchedUser,
+    });
   } catch (error) {
     res.status(500).json({ message: "INTERNAL SERVER ERROR" });
   }
 });
 
-app.use("/", (req, res) => res.status(404));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+);
 
 app.listen(API_PORT, () => console.log("Connected to 3001"));
